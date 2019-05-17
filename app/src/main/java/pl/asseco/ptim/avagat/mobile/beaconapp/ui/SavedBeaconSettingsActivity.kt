3b241c22095627f31e6a694c0bd64dcc697b18f1
oleksii.perets <oleksii.perets@asseco.pl>
2019-05-17 20:26:55 +0200

    Preparation for functionality of selecting actions for beacon getting close or far.

--- app/src/main/java/pl/asseco/ptim/avagat/mobile/beaconapp/ui/SavedBeaconSettingsActivity.kt
+++ app/src/main/java/pl/asseco/ptim/avagat/mobile/beaconapp/ui/SavedBeaconSettingsActivity.kt
@@ -1,13 +1,19 @@
 package pl.asseco.ptim.avagat.mobile.beaconapp.ui
 
+import android.drm.DrmStore
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import android.media.AudioManager
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import pl.asseco.ptim.avagat.mobile.beaconapp.R
 import pl.asseco.ptim.avagat.mobile.beaconapp.SMApp
 import pl.asseco.ptim.avagat.mobile.beaconapp.beacons.MyBeacon
+import pl.asseco.ptim.avagat.mobile.beaconapp.utils.Actions
 
 
 class SavedBeaconSettingsActivity : AppCompatActivity() {
@@ -22,10 +28,33 @@ class SavedBeaconSettingsActivity : AppCompatActivity() {
             supportActionBar?.title = thisBeacon.name
             supportActionBar?.subtitle = String.format("%.2f", thisBeacon.calibratedRssi)
         }
-        //TODO: to change beacon, onitemlongclick?
-        //changing ring sound mode
-        /*val amanager: AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
-        amanager.ringerMode = AudioManager.RINGER_MODE_VIBRATE*/
+        val spinnerIn: Spinner = findViewById(R.id.saved_beacon_settings_in_spinner)
+        spinnerIn.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            }
+
+        }
+        val spinnerOut: Spinner = findViewById(R.id.saved_beacon_settings_out_spinner)
+        spinnerOut.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                //
+            }
+
+        }
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, Actions.ACTION_LIST)
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        spinnerIn.adapter = adapter
+        spinnerOut.adapter = adapter
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
--- app/src/main/java/pl/asseco/ptim/avagat/mobile/beaconapp/utils/Actions.kt
+++ app/src/main/java/pl/asseco/ptim/avagat/mobile/beaconapp/utils/Actions.kt
@@ -11,18 +11,25 @@ class Actions(private val context: Context) {
         val ACTION_RINGER_MODE_VIBRATE_TAG = "ACTION_RINGER_MODE_VIBRATE_TAG"
         val ACTION_RINGER_MODE_NORMAL_TAG = "ACTION_RINGER_MODE_NORMAL_TAG"
         val ACTION_RINGER_MODE_SILENT_TAG = "ACTION_RINGER_MODE_SILENT_TAG"
+        val ACTION_LIST: ArrayList<String> = arrayListOf("Nothing", "Sound mode: Vibrate", "Sound mode: Normal", "Sound mode: Silent")
     }
 
     fun stateChangedAction(actionTag: String?){
         if(actionTag==null)
             return
-    }
-
-    fun beaconStateChange(isClose: Boolean) {
-        val amanager: AudioManager = context.getSystemService(AUDIO_SERVICE) as AudioManager
-        if(isClose)
-            amanager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
-        else
-            amanager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+        when(actionTag){
+            ACTION_RINGER_MODE_VIBRATE_TAG ->{
+                val amanager: AudioManager = context.getSystemService(AUDIO_SERVICE) as AudioManager
+                amanager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+            }
+            ACTION_RINGER_MODE_NORMAL_TAG -> {
+                val amanager: AudioManager = context.getSystemService(AUDIO_SERVICE) as AudioManager
+                amanager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+            }
+            ACTION_RINGER_MODE_SILENT_TAG -> {
+                val amanager: AudioManager = context.getSystemService(AUDIO_SERVICE) as AudioManager
+                amanager.ringerMode = AudioManager.RINGER_MODE_SILENT
+            }
+        }
     }
 }
